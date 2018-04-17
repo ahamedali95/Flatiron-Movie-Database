19faf5a08010b3e6eb144a4c3bcd76781888bc9d
rogercodes1 <roger.ucsb@gmail.com>
2018-04-17 19:28:31 -0400

    added options, sub_options, and goodbye

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,45 +1,66 @@
 require_relative "../config/environment.rb"
 #
 def list_commands
-  "Available commands: "
-  "  - List all commands [l]"
-  "  - Exit the program [e]"
-  "  - Info About a movie [title: ?]"
-  "  - Find actors' movies [actor: ?]"
-  "  - Find directors' movies [director: ?]"
-  "  - Find popular movie [popular]"
-  "  - Find oldest movie  [oldest]"
-  "  - Find highest grosser [highest]"
-  "  - Find latest release [latest]"
+  "*************************************************"
+  "  # 1. See list of Movies, Directors, Actors."
+  "  # 2. Search Online for Available movies."
+  "  # 3. Search Movies by Actor."
+  "  # 4. Search Movies by Director."
+  "  # 5. Top 3 rated Movies within our current db."
+  "  # 6. Top 3 Box Office Movies within current db."
+  "  # 7. Find Movies by MPAA Rating. Ex. PG-13"
+  "  # 8. Search Movie by by decade"
+  "  # 9. Search Movie by by Studio."
+  "*************************************************"
+  "Please enter an option from 1-9, 'e' to Exit. "
 end
 
-def welcome
-  puts "Welcome to mini-IMDB"
-  puts list_commands
-  input = gets.chomp().downcase
+def sub_options
+  puts "What would you like to do?"
+  puts "A. See List of Movies."
+  puts "B. See List of Directors."
+  puts "C. See List of Actors.\n"
+  puts "*** Press (e) to EXIT! ***"
+  input = gets.chomp
 
-  while input != "e"
-    if input == "l"
-      puts list_commands
-    elsif input == input.start_with("t")
-      #query the movie info in the database. If not exists, go query it from
-      #the API and seed to the database and then return the information
-    elsif input == input.start_with("a")
-      #SELECT * FROM actors
-      #INNER JOIN casts
-      #on actors.id = casts.actor_id
-      #WHERE movies.id = actors.movie_id
-    elsif input == input.start_with("d")
-      #SELECT * FROM directors
-      #INNER JOIN directed_movies
-      #on directors.id = directed_movies.director_id
-      #WHERE movies.id = directors.movie_id
+end
 
-    else
-      puts "Please follow the commands"
-    end
+
+
+def goodbye
+  puts "Thank you for stopping bye!!"
+  puts "GoodBye"
+  abort
+end
+
+def print_one_list(input)
+  case input
+
+
   end
 
+
+def options
+  input = gets.chomp
+
+  case input
+  when "e"
+    goodbye
+  when "1"
+    input = sub_options
+    print_one_list(input)
+    else
+      list_commands
+      options
+  end
+  2. Search available movies online
+  3. Search movies by actor
+  4. Search movies by director
+  5. Top 3 rated movies within our current database.
+  6. Top 3 Box Office movies within db
+  7. Find movies by MPAA Rating = PG-13
+  8. Look up movie by decade
+  9. Look up movies by studio.
 end
 
 #DO NOT CALL RUN in here.
