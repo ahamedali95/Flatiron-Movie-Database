9e063a7c805f4a80258f82be10e62a30c93357be
rogercodes1 <roger.ucsb@gmail.com>
2018-04-17 14:24:42 -0400

    updated info.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # Director.create("james madison")
 # #comment
+require_relative "../config/environment.rb"
 require "pry"
 require "rest-client"
 require "json"
 
 
-movies = ["Batman", "Superman", "Spiderman", "The Godfather", "Up",
+array = ["Batman", "Superman", "Spiderman", "The Godfather", "Up",
   "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man"]
 
 
 array.each do |film|
-
   r = RestClient.get("http://www.omdbapi.com/?t=#{film}&apikey=485b50f7")
   movie = JSON.parse(r)
   title = movie["Title"]
@@ -18,10 +18,12 @@ array.each do |film|
   rated = movie["Rated"]
   released = movie["Released"]
   genre = movie["Genre"]
+  director = movie["Director"]
   plot = movie["Plot"]
   imdb_rating = movie["imdbRating"]
   box_office = movie["BoxOffice"]
   production = movie["Production"]
+  Director.find_or_create_by(name: director)
   binding.pry
 end
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,15 @@
-
-
-puts "Welcome"
-
-puts "Enter a movie name: "
-input = gets.chomp
-
-response = RestClient.get("http://www.omdbapi.com/?t=#{input}&apikey=485b50f7")
-hash = JSON.parse(response)
-
-#hash["posts"][0]["thread"]["site"]
-
-
-binding.pry
-"hello"
+require_relative "../config/environment.rb"
+#
+# puts "Welcome"
+#
+# puts "Enter a movie name: "
+# input = gets.chomp
+#
+# response = RestClient.get("http://www.omdbapi.com/?t=#{input}&apikey=485b50f7")
+# hash = JSON.parse(response)
+#
+# #hash["posts"][0]["thread"]["site"]
+#
+#
+# binding.pry
+# "hello"
