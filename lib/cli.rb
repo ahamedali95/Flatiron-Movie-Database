0294433777b459ace24ce75948adef5fa0241c72
AHAMED ABBAS <ahamedali95@gmail.com>
2018-04-17 11:39:13 -0400

    Update cli.rb

---
+++ lib/cli.rb
@@ -0,0 +1,13 @@
+
+
+puts "Enter a movie name: "
+input = gets.chomp 
+
+response = RestClient.get("http://www.omdbapi.com/?t=#{input}&apikey=485b50f7")
+hash = JSON.parse(response)
+
+#hash["posts"][0]["thread"]["site"]
+
+
+binding.pry
+"hello"
