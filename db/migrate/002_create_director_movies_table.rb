879e0f9edc2300b8697c1a4054c99d0fc6597b1c
Ahamed <ahamedali95@gmail.com>
2018-04-17 11:59:44 -0400

    added comment

--- db/migrate/002_create_director_movies_table.rb
+++ db/migrate/002_create_director_movies_table.rb
@@ -3,6 +3,7 @@ class CreateDirectorMovieTable < ActiveRecord::Migration[5.1]
     create_table :movies_directed do |t|
       t.integer :director_id
       t.integer :movie_id
+      #comment
     end
   end
 end
