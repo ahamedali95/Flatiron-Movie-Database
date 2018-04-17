75ed4b5b2d1f954a73544357f111804b70b96671
rogercodes1 <roger.ucsb@gmail.com>
2018-04-17 14:15:36 -0400

    updated seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,24 @@ require "rest-client"
 require "json"
 
 
-movies = ["Batman", "Superman", "Spiderman", "The Godfather", "The Dark Knight Rises",
-  "The Dark Knight", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man"]
+movies = ["Batman", "Superman", "Spiderman", "The Godfather", "Up",
+  "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man"]
 
 
-array.each do |movie|
-  response = RestClient.get("http://www.omdbapi.com/?t=#{movie.split(" ").join("+")}&apikey=485b50f7")
-  hash = JSON.parse(response)
+array.each do |film|
+
+  r = RestClient.get("http://www.omdbapi.com/?t=#{film}&apikey=485b50f7")
+  movie = JSON.parse(r)
+  title = movie["Title"]
+  year = movie["Year"]
+  rated = movie["Rated"]
+  released = movie["Released"]
+  genre = movie["Genre"]
+  plot = movie["Plot"]
+  imdb_rating = movie["imdbRating"]
+  box_office = movie["BoxOffice"]
+  production = movie["Production"]
+  binding.pry
 end
 
 
