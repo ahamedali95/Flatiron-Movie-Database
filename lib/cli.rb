078b1bed6e44406afa2791d445d0720ee4baf39b
Ahamed <ahamedali95@gmail.com>
2018-04-17 14:12:02 -0400

    changes

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,24 +1,14 @@
-require 'pry'
-require 'json'
-require 'rest-client'
+
+
+puts "Welcome"
 
 puts "Enter a movie name: "
 input = gets.chomp
 
 response = RestClient.get("http://www.omdbapi.com/?t=#{input}&apikey=485b50f7")
-movie = JSON.parse(response)
+hash = JSON.parse(response)
 
-#movie["posts"][0]["thread"]["site"]
-
-title = movie["Title"]
-year = movie["Year"]
-rated = movie["Rated"]
-released = movie["Released"]
-genre = movie["Genre"]
-plot = movie["Plot"]
-imdb_rating = movie["imdbRating"]
-box_office = movie["BoxOffice"]
-production = movie["Production"]
+#hash["posts"][0]["thread"]["site"]
 
 
 binding.pry
