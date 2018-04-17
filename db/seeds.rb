1e3a069e6d475d8e3374af0b899b3abeb12dc9b5
rogercodes1 <roger.ucsb@gmail.com>
2018-04-17 17:06:18 -0400

    "figured out join table for director and movie.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ require "json"
 
 
 array = ["Batman", "Superman", "Spiderman", "The Godfather", "Up",
-  "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man"]
+  "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man","Nightmare Before Christmas", "Alice in wonderland"]
 
-array.each do |film|
+array.each do |film|j
   r = RestClient.get("http://www.omdbapi.com/?t=#{film}&apikey=485b50f7")
   movie = JSON.parse(r)
   title = movie["Title"]
@@ -17,16 +17,17 @@ array.each do |film|
   rated = movie["Rated"]
   released = movie["Released"]
   genre = movie["Genre"]
-  director = movie["Director"]
+  director = movie["Director"].split(",").first
   plot = movie["Plot"]
   rating = movie["imdbRating"]
   box_office = movie["BoxOffice"]
   production = movie["Production"]
   d = Director.find_or_create_by(name: director)
   m = Movie.find_or_create_by(title: title, year: year, rated: rated, released: released, genre: genre, plot: plot, rating: rating, box_office: box_office, production: production)
-  binding.pry
-  dm_join = DirectedMovie.find_or_create_by(director_id: d, movie_id: m)
+  dm_join = DirectedMovie.find_or_create_by(director_id: d.id, movie_id: m.id)
+
 end
+
 
 # binding.pry
 
