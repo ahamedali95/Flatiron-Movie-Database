67a71fce8550833a1bd1e51795635e609da5687c
Ahamed <ahamedali95@gmail.com>
2018-04-18 09:52:16 -0400

    added a method

--- lib/cli.rb
+++ lib/cli.rb
@@ -25,72 +25,72 @@ def print_list_commands
   "*************************************************"
   "Please enter an option from 1-9, 'e' to Exit. "
 end
-# 1a. See Available movies within database
-def get_movie_infor_from_db
+# # 1a. See Available movies within database
+def get_movie_info_from_db
   Movie.select(:title).map do |movie_obj|
-    movie_obj.title
+    puts movie_obj.title
   end
 end
-# 1b. See available actors.
-Actor.select(:name).map do |actor_obj|
-  actor_obj.name
-end
-# 1c. See available directors.
-Director.select(:name).map do |director_obj|
-  director_obj.name
-end
-# 2. Search available movies online
-# 3. Search movies by actor
-
-#Movie.joins(casts: :actor).where("actors.name = ?, 'Marlon Brando'")
-
-# SELECT movies.name FROM movies
-# INNER JOIN casts
-# ON movies.id = movie_id
-# INNER JOIN actors
-# ON casts.actor_id = actors.id
-# WHERE actors.name = "hugh jackman"
-
-
-# 4. Search movies by director
-# Movie.where(director: <input>) <-- needs error handling, will do tomorrow -MDT
-# 5. Top 3 rated movies within our current database.
-movies = Movie.order("order DESC")
-[movies[0], movies[1], movies[2]]
-
-# Movie.order(rating: :desc).limit(3)
-
-# 6. Top 3 Box Office movies within db
-# Movie.order(box_office: :desc).limit(3)
-# 7. Find movies by MPAA Rating = PG-13
-Movie.where(rated: "PG-13")
-# 8. Look up movie by decade
-
-# 9. Look up movies by studio.
-#SELECT * FROM movies WHERE production = "green studios"
-Movie.where(production: "whatever")
-
-
-  while input != "e"
-    if input == "l"
-      puts list_commands
-    elsif input == input.start_with("t")
-      #query the movie info in the database. If not exists, go query it from
-      #the API and seed to the database and then return the information
-    elsif input == input.start_with("a")
-      #SELECT * FROM actors
-      #INNER JOIN casts
-      #on actors.id = casts.actor_id
-      #WHERE movies.id = actors.movie_id
-    elsif input == input.start_with("d")
-      #SELECT * FROM directors
-      #INNER JOIN directed_movies
-      #on directors.id = directed_movies.director_id
-      #WHERE movies.id = directors.movie_id
-# Movie.where(rated: <input>) <-- needs error handling in case some smartass puts in XXX - MDT
-# 8. Look up movie by decade
-# This one's gonna take some doing, but I have a few ideas. -MDT
-# 9. Look up movies by studio.
+# # 1b. See available actors.
+# Actor.select(:name).map do |actor_obj|
+#   actor_obj.name
+# end
+# # 1c. See available directors.
+# Director.select(:name).map do |director_obj|
+#   director_obj.name
+# end
+# # 2. Search available movies online
+# # 3. Search movies by actor
+#
+# #Movie.joins(casts: :actor).where("actors.name = ?, 'Marlon Brando'")
+#
+# # SELECT movies.name FROM movies
+# # INNER JOIN casts
+# # ON movies.id = movie_id
+# # INNER JOIN actors
+# # ON casts.actor_id = actors.id
+# # WHERE actors.name = "hugh jackman"
+#
+#
+# # 4. Search movies by director
+# # Movie.where(director: <input>) <-- needs error handling, will do tomorrow -MDT
+# # 5. Top 3 rated movies within our current database.
+# movies = Movie.order("order DESC")
+# [movies[0], movies[1], movies[2]]
+#
+# # Movie.order(rating: :desc).limit(3)
+#
+# # 6. Top 3 Box Office movies within db
+# # Movie.order(box_office: :desc).limit(3)
+# # 7. Find movies by MPAA Rating = PG-13
+# Movie.where(rated: "PG-13")
+# # 8. Look up movie by decade
+#
+# # 9. Look up movies by studio.
+# #SELECT * FROM movies WHERE production = "green studios"
+# Movie.where(production: "whatever")
+#
+#
+#   while input != "e"
+#     if input == "l"
+#       puts list_commands
+#     elsif input == input.start_with("t")
+#       #query the movie info in the database. If not exists, go query it from
+#       #the API and seed to the database and then return the information
+#     elsif input == input.start_with("a")
+#       #SELECT * FROM actors
+#       #INNER JOIN casts
+#       #on actors.id = casts.actor_id
+#       #WHERE movies.id = actors.movie_id
+#     elsif input == input.start_with("d")
+#       #SELECT * FROM directors
+#       #INNER JOIN directed_movies
+#       #on directors.id = directed_movies.director_id
+#       #WHERE movies.id = directors.movie_id
+# # Movie.where(rated: <input>) <-- needs error handling in case some smartass puts in XXX - MDT
+# # 8. Look up movie by decade
+# # This one's gonna take some doing, but I have a few ideas. -MDT
+# # 9. Look up movies by studio.
 
 
 def sub_options
@@ -104,8 +104,6 @@ def sub_options
   input = gets.chomp.downcase
 end
 
-
-end
 
 def goodbye
   puts "Thank you for stopping bye!!"
@@ -129,9 +127,9 @@ def print_one_list(input)
     else
       input = sub_options
       print_one_list(input)
-    end
-
   end
+end
+
 
 
 
@@ -148,7 +146,7 @@ def options
       puts "Please enter a movie title: \n"
       input = gets.chomp
 
-      # method(input)
+      get_movie_info_from_db
     when "3"
       puts "Please enter an actors name: \n"
       input = gets.chomp
