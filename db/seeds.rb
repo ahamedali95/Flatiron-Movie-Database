a414986b12f997bf0b303fa0f9cb275965e55dd4
Ahamed <ahamedali95@gmail.com>
2018-04-18 00:00:23 -0400

    cli.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,38 @@ require "rest-client"
 require "json"
 
 
-array = ["Batman", "Superman", "Spider-man", "The Godfather", "Up", "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar", "Iron Man","Nightmare Before Christmas", "Alice in wonderland","Jurassic Park", "Jurassic World", "Batman Returns", "Spider-Man 2", "Ultimate Spider-Man","Iron-man","Iron-man 2", "Iron-man 3",
+array = ["Batman", "Superman", "Spider-man", "The Godfather", "Up",
+  "The Lion King", "Prisoners", "The Dark Knight Rises", "Avatar",
+  "Iron Man","Nightmare Before Christmas", "Alice in wonderland",
+  "Jurassic Park", "Jurassic World", "Batman Returns", "Spider-Man 2",
+  "Ultimate Spider-Man","Iron-man","Iron-man 2", "Iron-man 3",
   "Spider-Man 3", "The Lost World: Jurassic Park", "Jurassic Park III"]
+  #binding.pry
 
 array.each do |film|j
   r = RestClient.get("http://www.omdbapi.com/?t=#{film}&apikey=485b50f7")
   movie = JSON.parse(r)
-  binding.pry
   title = movie["Title"]
-  year = movie["Year"]
+  year = movie["Year"].to_i
   rated = movie["Rated"]
   released = movie["Released"]
   genre = movie["Genre"]
   director = movie["Director"].split(",").first
   plot = movie["Plot"]
-  rating = movie["imdbRating"]
+  #rating is a floating value but it goes in to the database as a real value
+  rating = movie["imdbRating"].to_f
   box_office = movie["BoxOffice"]
   production = movie["Production"]
 
   director = Director.find_or_create_by(name: director)
-  movie = Movie.find_or_create_by(title: title, year: year.to_i, rated: rated, released: released, genre: genre, plot: plot, rating: rating, box_office: box_office, production: production)
+  movie = Movie.find_or_create_by(title: title, year: year, rated: rated, released: released, genre: genre, plot: plot, rating: rating, box_office: box_office, production: production)
   directed_movie = DirectedMovie.find_or_create_by(director_id: director.id, movie_id: movie.id)
 
-  actors = movie["Actors"].split(",")
-    actors.each do |name|
-      actor = Actor.find_or_create_by(name: name)
-      cast = Cast.find_or_create_by(actor_id: actor.id, movie_id: movie.id)
-    end
+  # actors = movie["Actors"].split(",")
+  #   actors.each do |name|
+  #     actor = Actor.find_or_create_by(name: name)
+  #     cast = Cast.find_or_create_by(actor_id: actor.id, movie_id: movie.id)
+  #   end
   #dm_join = DirectedMovie.find_or_create_by(director_id: director.id, movie_id: movie.id)
 
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -14,21 +14,42 @@ def list_commands
   "  - Find latest release [latest]"
 end
 # 1a. See Available movies within database
+Movie.select(:title).map do |movie_obj|
+  movie_obj.title
+end
 # 1b. See available actors.
+Actor.select(:name).map do |actor_obj|
+  actor_obj.name
+end
 # 1c. See available directors.
+Director.select(:name).map do |director_obj|
+  director_obj.name
+end
 # 2. Search available movies online
 # 3. Search movies by actor
 
-# SELECT movies.name FROM movies
+#Movie.joins(casts: :actor).where("actors.name = ?, 'Marlon Brando'")
 
-# Movie.joins(:casts, :actors).WHERE("casts.actor_id = actors.id AND actors.name = ' Chris Sarando'")
-# joins("INNER JOIN actors ON casts.actor_id = actors.id WHERE actors.name = ' Chris Sarando'")
+# SELECT movies.name FROM movies
+# INNER JOIN casts
+# ON movies.id = movie_id
+# INNER JOIN actors
+# ON casts.actor_id = actors.id
+# WHERE actors.name = "hugh jackman"
+
+
 # 4. Search movies by director
 # 5. Top 3 rated movies within our current database.
+movies = Movie.order("order DESC")
+[movies[0], movies[1], movies[2]]
 # 6. Top 3 Box Office movies within db
 # 7. Find movies by MPAA Rating = PG-13
+Movie.where(rated: "PG-13")
 # 8. Look up movie by decade
+
 # 9. Look up movies by studio.
+#SELECT * FROM movies WHERE production = "green studios"
+Movie.where(production: "whatever")
 
 
 
